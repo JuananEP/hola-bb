a3ad67b46e815e3eac97df4302432b814f4e5a13
Juanan <al375542@uji.es>
2019-09-12 16:18:05 +0200

    introduccion de nombre

--- hello.java
+++ hello.java
@@ -1,6 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
 public class Main
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main (String[]args)
+  {
+    String entradaTeclado = "";
+      System.out.print ("Introduce tu puto nombre: ");
+    Scanner entradaEscaner = new Scanner (System.in);
+      entradaTeclado = entradaEscaner.nextLine ();
+
+      System.out.println ("Hey " + entradaTeclado);
+  }
 }
